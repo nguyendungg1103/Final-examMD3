e676f9d8d6a0a48acfb6752807be7c4a895251a9
nguyendungg1103 <nguyendungg1103@gmail.com>
2022-06-27 11:36:43 +0700

    update

--- model/script.js
+++ model/script.js
@@ -18,5 +18,13 @@ module.exports = {
                 resolve('add success')
             })
         })
-    }
-    }
+
+    }, edit: (data, id) => {
+        const editSql = `UPDATE new_table SET nameCity = '${data.nameCity}',nameCountry = '${data.nameCountry}', dientich = ${Number(data.dientich)}, danso = ${Number(data.danso)}, gdp = ${Number(data.gdp)}, gioithieu = '${data.gioithieu}' WHERE new_table.id = '${id}'`;
+        return new Promise((resolve, reject) => {
+            conn.query(editSql, err => {
+                if (err) reject(err);
+                resolve('edit success');
+            });
+        })}
+}
